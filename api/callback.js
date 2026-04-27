e305d963408ec5059634c119f9250fe27958533f
sebastianmlk9academy <sebastian.mlk9academy@gmail.com>
2026-04-27 18:29:54 +0200

    Create callback.js

---
+++ api/callback.js
@@ -0,0 +1,58 @@
+module.exports = async (req, res) => {
+  try {
+    const requestUrl = new URL(req.url, "https://dummy.local");
+    const code = requestUrl.searchParams.get("code");
+    const state = requestUrl.searchParams.get("state");
+
+    const cookie = req.headers.cookie || "";
+    const match = cookie.match(/(?:^|;\\s*)decap_oauth_state=([^;]+)/);
+    const savedState = match ? decodeURIComponent(match[1]) : null;
+
+    if (!state || !savedState || state !== savedState) {
+      res.statusCode = 401;
+      return res.end("Invalid OAuth state");
+    }
+
+    const tokenResp = await fetch("https://github.com/login/oauth/access_token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code
+      })
+    });
+
+    const tokenData = await tokenResp.json();
+
+    if (!tokenData.access_token) {
+      res.statusCode = 500;
+      return res.end("No access token from GitHub");
+    }
+
+    const origin = process.env.ORIGIN;
+
+    res.setHeader("Content-Type", "text/html");
+    res.end(`<!doctype html>
+<html>
+<body>
+<script>
+  (function() {
+    var msg = 'authorization:github:success:' + JSON.stringify({
+      token: '${tokenData.access_token}',
+      provider: 'github'
+    });
+    window.opener && window.opener.postMessage(msg, '${origin}');
+    window.close();
+  })();
+</script>
+</body>
+</html>`);
+  } catch (e) {
+    res.statusCode = 500;
+    res.end("OAuth callback error");
+  }
+};
